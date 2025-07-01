73bb743b3a4e7819a6e42c4ae4eff3feb6c28cb7
Daniela <danielarodrigues2po@gmail.com>
2025-07-01 09:44:43 -0300

    Rota e controller editar voluntario

--- backEnd/src/routes/index.js
+++ backEnd/src/routes/index.js
@@ -4,12 +4,14 @@ const router = express.Router();
 // Controllers
 const controleVoluntario = require('../controllers/voluntariosController');
 const controleCertificado = require('../controllers/certificadoController');
+const voluntariosController = require('../controllers/voluntariosController');
 
 //Routers voluntqarios
 router.post('/voluntarios', controleVoluntario.criarVoluntario);
 router.get('/voluntarios', controleVoluntario.getAllVoluntarios)
 router.get('/voluntarios/:idVoluntario', controleVoluntario.getVoluntario)
 router.delete('/voluntarios/:idVoluntario', controleVoluntario.deletaVoluntario)
+router.put('/voluntarios/:idVoluntario', voluntariosController.editarVoluntario)
 
 //Routers certificados
 router.post('/voluntarios/:idVoluntario/certificados', controleCertificado.criarCertificado);
