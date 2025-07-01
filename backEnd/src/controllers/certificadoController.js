6ecca60a3f560c17c4caa78e105ec2613e8928db
Marcelo Gregório Vieira <marcelovieira8967@gmail.com>
2025-07-01 13:47:27 -0300

    fix certificado

--- backEnd/src/controllers/certificadoController.js
+++ backEnd/src/controllers/certificadoController.js
@@ -190,7 +190,7 @@ module.exports = {
         );
         doc.moveDown(2);
         doc.text(`Data de Emissão: ${cert.dataEmissao}`, { align: "right" });
-        doc.text("Coordenação de Extensão - UTFPR", { align: "right" });
+        doc.text("Coordenação ELLP - UTFPR", { align: "right" });
 
         doc.end();
     }
